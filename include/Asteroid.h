ee73e63fa4b629105b71b25ab71a5d03353d0884
huzaifa-ali05 <huzaifasiddiquiali@gmail.com>
2025-11-16 22:44:53 +0500

    Huzaifa Classes

---
+++ include/Asteroid.h
@@ -0,0 +1,21 @@
+#ifndef ASTEROID_H
+#define ASTEROID_H
+
+#include "Entity.h"
+
+
+
+class asteroid : public Entity {
+public:
+    int lives;
+    asteroid();
+    void update() override;
+};
+
+
+#endif
+
+
+//This file creates the Asteroid class, which is a special type of Entity. 
+//It tells the program that every asteroid in the game will have its own movement and behavior. 
+//The header just declares what the class can do — for example, it says that asteroids will have an update() function that will be defined later.
