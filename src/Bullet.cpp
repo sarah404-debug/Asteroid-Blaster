54bdee4e3aa2042037259835aa963922909ea660
huzaifa-ali05 <huzaifasiddiquiali@gmail.com>
2025-11-16 22:43:50 +0500

    Huzaifa Classes
    This file defines the bullet class, including its constructor and update method to handle movement and lifespan.

---
+++ src/Bullet.cpp
@@ -0,0 +1,38 @@
+#include "Bullet.h"
+#include <cmath>
+#include "Globals.cpp"
+
+
+
+
+bullet::bullet()
+{
+    name = "bullet";
+}
+
+void bullet::update()
+{
+    dx = cos(angle * DEGTORAD) * 6;
+    dy = sin(angle * DEGTORAD) * 6;
+    x += dx;
+    y += dy;
+
+    if (x > W || x < 0 || y > H || y < 0)
+        life = 0;
+}
+
+
+
+//This file explains how bullets behave.
+
+// When a bullet is created, it is given the name "bullet" so the game can identify it.
+
+// In the update() function:
+
+// dx and dy are calculated using the bullet’s angle so it moves in the direction the player is pointing.
+
+// The bullet’s position (x and y) is updated each frame to make it move smoothly.
+
+// If the bullet goes off the screen edges (W and H), it is marked as dead (life = 0) so the game can remove it.
+
+// In short, this file makes bullets fly in the correct direction and disappear when they leave the screen
