df53bf0b2eab64a9706d967bb564d2ac6c6c5226
huzaifa-ali05 <huzaifasiddiquiali@gmail.com>
2025-12-06 21:50:17 +0500

    Huzaifa Classes

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -4,18 +4,18 @@
 
 Bullet::Bullet()
 {
-    name = "bullet";
+    name = "bullet";   // Set object name
 }
 
 void Bullet::update()
 {
-    dx = cos(angle * DEGTORAD) * 6;
-    dy = sin(angle * DEGTORAD) * 6;
+    dx = cos(angle * DEGTORAD) * 6;   // horizontal velocity based on angle
+    dy = sin(angle * DEGTORAD) * 6;   //vertical velocity based on angle
 
-    x += dx;
-    y += dy;
+    x += dx;   // moe bullet on x-axis
+    y += dy;   // move bullet on y-axis
 
+    // bullet leaves screen bounds, destroy 
     if (x > W || x < 0 || y > H || y < 0)
         life = false;
 }
-
